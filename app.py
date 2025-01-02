7efdf950c2cf25e34fa3ffbc7ea6ffd3a8098ed6
Ali Abdullah <intern.ali@devsinc.com>
2025-01-02 18:41:37 +0500

    update

--- app.py
+++ app.py
@@ -1,17 +1,12 @@
 from flask import Flask, render_template, request, jsonify
 from chat import get_response
 from flask_cors import CORS
-import toml
-
-# with open("config.toml", "r") as config_file:
-#     config = toml.load(config_file)
 
 
 app = Flask(__name__, 
             static_folder="static",  # Folder for CSS, JS, images
             template_folder="templates" ) # Folder for HTML templates)
 
-# CORS(app, resources={r"/*": {"origins": config["app"]["cors_allowed_origins"]}})
 
 @app.get("/")
 def index_get():
@@ -28,4 +23,4 @@ def predict():
 
 if __name__ == "__main__":
     from waitress import serve
-    serve(app, host="0.0.0.0", port=8080)
+    serve(app, host="0.0.0.0", port=50100, threads=2)
