f45dd82521001b823199e50fd0180aadf0bdb0f2
Ali Abdullah <intern.ali@devsinc.com>
2025-01-02 21:24:15 +0500

    update

--- app.py
+++ app.py
@@ -25,4 +25,4 @@ def predict():
 
 if __name__ == "__main__":
     from waitress import serve
-    serve(app, host="0.0.0.0", port=50100, threads=2)
+    serve(app, host="0.0.0.0", port=8000, threads=2)
