e592430e5b0907605ddcba2954120820dfcc07d2
Ali Abdullah <intern.ali@devsinc.com>
2025-01-02 20:02:23 +0500

    Update app.py

--- app.py
+++ app.py
@@ -23,6 +23,6 @@ def predict():
 
 
 if __name__ == "__main__":
-    app.run()
-    # http_server = WSGIServer(('', 5000), app)
-    # http_server.serve_forever()
+    # app.run(debug=True)
+    http_server = WSGIServer(('', 5000), app)
+    http_server.serve_forever()
