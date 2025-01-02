5e64352723cf08287d11da20683673ffa83905f2
Ali Abdullah <intern.ali@devsinc.com>
2025-01-02 19:44:08 +0500

    Update app.py

--- app.py
+++ app.py
@@ -19,3 +19,8 @@ def predict():
     response = get_response(text)
     message = {"answer": response}
     return jsonify(message)
+
+
+if __name__ == "__main__":
+    from waitress import serve
+    serve(app, host="0.0.0.0", port=50100, threads=2)
